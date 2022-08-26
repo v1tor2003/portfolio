ab67d53dd9ac96e1b6b72abd23a259ebafd2fece
v1tor2003 <vitor.pr04@hotmail.com>
2022-08-26 17:14:13 -0300

    about-done

---
+++ portfolio-site/src/components/About/Profile.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import ProfilePhoto from '../../images/profile.jpeg'
+import styles from './About.module.scss'
+
+export const Profile: React.FC<{}> = () => {
+  const btnText: string= `Let's talk`
+  
+  return (
+    <React.Fragment>
+      <div className={styles.pic}>
+        <img src={ProfilePhoto} alt='profile'/>
+        <h4 className={styles.name}>Vitor Pires</h4>
+        <span className={styles.addr}>Ilheus, Brazil</span>
+        <p className={styles.role}>Web dev | game dev</p>
+        <Link to='/contact'><button>{btnText}</button></Link> 
+      </div>
+    </React.Fragment>
+  )
+}
