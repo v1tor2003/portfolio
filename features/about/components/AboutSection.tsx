6863af7c19af8c6275c8e3deb1e9dc73e7220cb2
Vítor Pires <vitor.pr04@hotmail.com>
2026-09-10 12:03:39 -0300

    feat(about): add black background to about content section to block background rain

--- features/about/components/AboutSection.tsx
+++ features/about/components/AboutSection.tsx
@@ -4,10 +4,7 @@ import { TechStackGrid } from "./TechStackGrid";
 
 export function AboutSection() {
 	return (
-		<section
-			id="about"
-			className="px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto w-full scroll-mt-20 space-y-12"
-		>
+		<section id="about" className="w-full scroll-mt-20 space-y-12">
 			<AboutHeader />
 			<CoreHighlights />
 			<TechStackGrid />
